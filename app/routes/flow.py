9d8b167f98a53c3f717428726b5b9ae4630f55d0
Timur Gafarov <gecko0307@gmail.com>
2021-02-23 22:30:13 +0300

    Estimate annual flow based on previous years

--- app/routes/flow.py
+++ app/routes/flow.py
@@ -27,12 +27,20 @@ def flowJson(db):
         'expense': [0 for x in range(12)],
         'net': [0 for x in range(12)]
     }
-    year = datetime.now().year
+    currentDateTime = datetime.now()
+    currentYear = currentDateTime.year
+    year = currentYear
     if 'year' in request.query:
-        year = request.query['year']
-    query = select('Transactions', ['strftime(\'%Y\', DATETIME) = \'' + str(year) + '\''])
+        year = int(request.query['year'])
+    query = select('Transactions')
+    # query = select('Transactions', ['strftime(\'%Y\', DATETIME) = \'' + str(year) + '\''])
     query += ' ORDER BY CAST(strftime(\'%m\', DATETIME) AS INTEGER)'
     cursor = db.execute(query)
+    
+    fullRevenue = 0
+    fullExpence = 0
+    startYear = currentYear
+    
     for trans in cursor.fetchall():
         if trans['CATEGORY'] != 'notrack':
             d = datetime.strptime(trans['DATETIME'], '%Y-%m-%d %H:%M:%S')
@@ -42,9 +50,51 @@ def flowJson(db):
             account = cursor.fetchall()[0]
             value = trans['VALUE']
             value *= account['REPORT_EXCHANGE_RATE']
+            
+            if d.year == year:
+                if value > 0:
+                    data['revenue'][month] += value
+                else:
+                    data['expense'][month] += -value
+                data['net'][month] += value
+            
             if value > 0:
-                data['revenue'][month] += value
+                fullRevenue += value
             else:
-                data['expense'][month] += -value
-            data['net'][month] += value
+                fullExpence += -value
+            
+            y = int(d.year)
+            if y < startYear:
+                startYear = y
+    
+    annualRevenue = sum(data['revenue'])
+    annualExpence = sum(data['expense'])
+    annualNet = annualRevenue - annualExpence
+    data['annualRevenue'] = annualRevenue
+    data['annualExpence'] = annualExpence
+    data['annualNet'] = annualNet
+    
+    reportYear = year;
+    numMonths = 0
+    if currentYear == reportYear:
+        numMonths = currentDateTime.month;
+    else:
+        numMonths = 12;
+    monthlyRevenue = annualRevenue / numMonths;
+    monthlyExpence = annualExpence / numMonths;
+    monthlyNet = annualNet / numMonths;
+    data['monthlyRevenue'] = monthlyRevenue
+    data['monthlyExpence'] = monthlyExpence
+    data['monthlyNet'] = monthlyNet
+    
+    numYearsFull = currentYear - startYear
+    numMonthsFull = numYearsFull * 12 + currentDateTime.month
+    estimatedAnnualRevenue = (fullRevenue / numMonthsFull) * 12
+    estimatedAnnualExpence = (fullExpence / numMonthsFull) * 12
+    estimatedAnnualNet = (estimatedAnnualRevenue - estimatedAnnualExpence)
+    data['estimatedAnnualRevenue'] = estimatedAnnualRevenue
+    data['estimatedAnnualExpence'] = estimatedAnnualExpence
+    data['estimatedAnnualNet'] = estimatedAnnualNet
+    
     return data
+
