0180097a5c61ea488e7626f39390c5f529c43405
Timur Gafarov <gecko0307@gmail.com>
2021-06-02 10:46:02 +0300

    Random port

--- wallet.py
+++ wallet.py
@@ -7,6 +7,7 @@ sys.path.append(os.getcwd())
 
 import io
 import threading
+import socket
 
 import bottle
 from bottle import request
@@ -17,10 +18,18 @@ from app.routes import *
 
 bottle.install(SQLitePlugin(dbfile = Config.databasePath))
 
-def serverMain():
-    bottle.run(host = '0.0.0.0', port = 8080)
+def randomPort():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("127.0.0.1", 0))
+    port = s.getsockname()[1]
+    s.close()
+    return port
 
-serverThread = threading.Thread(target=serverMain)
+def serverMain(port):
+    bottle.run(host = "0.0.0.0", port = port)
+
+port = randomPort()
+serverThread = threading.Thread(target=serverMain, args=(port,))
 serverThread.daemon = True
 serverThread.start()
 
@@ -28,10 +37,11 @@ from cefpython3 import cefpython as cef
 import platform
 import sys
 
-def cefMain():
+def cefMain(port):
     sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
     cef.Initialize()
-    browser = cef.CreateBrowserSync(url="http://localhost:8080/", window_title="Wallet")
+    url = "http://localhost:%s/" % port
+    browser = cef.CreateBrowserSync(url = url, window_title = "Wallet")
     browser.SetClientHandler(LifespanHandler())
     cef.MessageLoop()
     cef.Shutdown()
@@ -40,4 +50,4 @@ class LifespanHandler(object):
     def OnBeforeClose(self, browser):
         print("shutdown")
 
-cefMain()
+cefMain(port)
